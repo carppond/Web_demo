9095432f3681fd824c2ba72a26dc12ec041c810f
lixianshen <810646506@qq.com>
2022-05-04 22:51:56 +0800

    更新文档

--- toutiao-mobile/src/main.js
+++ toutiao-mobile/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式表
+import '@/style/index.css'
 
 Vue.config.productionTip = false
 
