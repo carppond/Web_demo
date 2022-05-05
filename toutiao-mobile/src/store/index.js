7bd37be589974959c4ef000f6456e24907fbc30e
lcf <objccf@gmail.com>
2022-05-05 18:46:03 +0800

    登录信息本地存储

--- toutiao-mobile/src/store/index.js
+++ toutiao-mobile/src/store/index.js
@@ -3,15 +3,18 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const TOKEN_KEY = 'TOUTIAO_USER'
 export default new Vuex.Store({
   state: {
-    user: null
+    user: JSON.parse(window.localStorage.getItem(TOKEN_KEY))
   },
   getters: {
   },
   mutations: {
     setUser (state, data) {
       state.user = data
+      // 为了防止刷新丢失,需要持久化
+      window.localStorage.setItem(TOKEN_KEY, JSON.stringify(data))
     }
   },
   actions: {
