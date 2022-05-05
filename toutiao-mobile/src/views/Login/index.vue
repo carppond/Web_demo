af9760fd10e494013b8c0dcb1a36d32a8460a245
lcf <objccf@gmail.com>
2022-05-05 15:45:32 +0800

    登录页面样式

--- toutiao-mobile/src/views/Login/index.vue
+++ toutiao-mobile/src/views/Login/index.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="login-container">
     <!-- 导航栏 -->
-    <van-nav-bar title="登录" />
+    <van-nav-bar class="page-nav-bar" title="登录" />
     <!-- 表单 -->
     <van-form @submit="onSubmit">
       <van-field
         name="用户名"
         placeholder="请输入手机号"
-      />
+      >
+      <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
       <van-field
         type="password"
-        name="密码"
+        name="验证码"
         placeholder="请输入验证码"
-      />
-      <div style="margin: 16px">
-        <van-button block type="info" native-type="submit"
+      >
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
+        </template>
+      </van-field>
+
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit"
           >登录</van-button
         >
       </div>
@@ -53,4 +61,24 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+  .send-sms-btn {
+    width: 160px;
+    height: 46px;
+    line-height: 46px;
+    font-size: 22px;
+     color: #999;
+    background-color: #ededed;
+  }
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
 </style>
