fb65b7a2859079d9e80317c17a6564c3981f2cb9
lcf <objccf@gmail.com>
2022-05-05 09:59:56 +0800

    引入 vant 组件库

--- toutiao-mobile/src/main.js
+++ toutiao-mobile/src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载 vant 核心组件库
+import Vant from 'vant'
+// 加载 vant 全局样式
+import 'vant/lib/index.css'
 // 加载全局样式表
 import '@/style/index.css'
+// 注册使用组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
